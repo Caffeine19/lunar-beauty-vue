44629c7edcd974d75ac0eaf4b21f32cfe78ff4f3
xin.su <939597201@qq.com>
2022-11-21 15:26:05 +0800

    完成LunarDivider的抽离和封装;
    拖了这么久，终于老实写哩.

---
+++ src/components/LunarDivider.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="border-b-[1px] h-1[1px]"
+    :class="[margin, dividerColor, width]"
+  ></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
+    margin: {
+      type: String,
+      default: "my-1",
+    },
+    dividerColor: {
+      type: String,
+      default: "border-zinc-700",
+    },
+    width: {
+      type: String,
+      default: "w-full",
+    },
+  },
+});
+</script>
+
+<style scoped></style>
